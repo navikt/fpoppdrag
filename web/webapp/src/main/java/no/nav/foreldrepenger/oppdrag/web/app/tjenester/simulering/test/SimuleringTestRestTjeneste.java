650e852f5dede31f4330a05b9677265a9535fd6f
Joakim Schweizer <joakim.schweizer@nav.no>
2020-04-29 16:32:41 +0200

    FIX for swagger after dependency bump

--- web/webapp/src/main/java/no/nav/foreldrepenger/oppdrag/web/app/tjenester/simulering/test/SimuleringTestRestTjeneste.java
+++ web/webapp/src/main/java/no/nav/foreldrepenger/oppdrag/web/app/tjenester/simulering/test/SimuleringTestRestTjeneste.java
@@ -51,7 +51,7 @@ public class SimuleringTestRestTjeneste {
     @Operation(description = "Lagre simulering test data", summary = ("Brukes til intern testing"), tags = "simtest")
     @BeskyttetRessurs(action = CREATE, ressurs = FAGSAK)
     public Response lagreSimuleringTestData(@NotNull @Valid SimuleringGjelderDto simuleringGjelderDto) {
-        if (true) {
+        if (unleash.isEnabled("fpoppdrag.testgrensesnitt")) {
             if (erProduksjonsmiljøet()) {
                 throw new IllegalStateException("fpoppdrag.testgrenesesnitt kan ikke aktiveres i produksjonsmijøet");
             }
