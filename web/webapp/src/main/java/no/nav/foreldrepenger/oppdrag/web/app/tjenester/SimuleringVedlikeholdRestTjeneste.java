e4a196d315963767804c848661dc13fbbf5a0d56
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2020-06-26 13:56:54 +0200

    Fix: manglet @Path-annotering

--- web/webapp/src/main/java/no/nav/foreldrepenger/oppdrag/web/app/tjenester/SimuleringVedlikeholdRestTjeneste.java
+++ web/webapp/src/main/java/no/nav/foreldrepenger/oppdrag/web/app/tjenester/SimuleringVedlikeholdRestTjeneste.java
@@ -18,6 +18,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
@@ -28,6 +29,8 @@ import no.nav.vedtak.sikkerhet.abac.BeskyttetRessurs;
 
 @ApplicationScoped
 @Transactional
+@Path("/forvaltning")
+@Produces(MediaType.APPLICATION_JSON)
 public class SimuleringVedlikeholdRestTjeneste {
 
     private static final Logger logger = LoggerFactory.getLogger(SimuleringVedlikeholdRestTjeneste.class);
