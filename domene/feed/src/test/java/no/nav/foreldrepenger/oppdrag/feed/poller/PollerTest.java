f3f296b23b847cfd21ada7097d84a54624c8e0fd
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2020-12-22 22:21:34 +0100

    NOJIRA fix broken test

--- domene/feed/src/test/java/no/nav/foreldrepenger/oppdrag/feed/poller/PollerTest.java
+++ domene/feed/src/test/java/no/nav/foreldrepenger/oppdrag/feed/poller/PollerTest.java
@@ -8,9 +8,12 @@ import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import no.nav.foreldrepenger.oppdrag.dbstoette.EntityManagerAwareExtension;
 import no.nav.vedtak.log.util.MemoryAppender;
 
+@ExtendWith(EntityManagerAwareExtension.class)
 public class PollerTest {
 
     public MemoryAppender logSniffer = MemoryAppender.sniff(Poller.class);
