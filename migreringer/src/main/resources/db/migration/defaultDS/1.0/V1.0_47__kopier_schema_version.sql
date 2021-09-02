a1e220f14cd216df1bc490bef920325d42f2cda4
Michal J. Sladek <michal.sladek@nav.no>
2021-09-02 12:59:31 +0200

    Fixer migrering om schema_version ikke finnes

--- migreringer/src/main/resources/db/migration/defaultDS/1.0/V1.0_47__kopier_schema_version.sql
+++ migreringer/src/main/resources/db/migration/defaultDS/1.0/V1.0_47__kopier_schema_version.sql
@@ -4,6 +4,15 @@ begin
     select count(*) into c from user_tables where table_name = 'schema_version';
     if c = 1 then
         EXECUTE IMMEDIATE 'CREATE TABLE flyway_schema_history AS (SELECT * FROM schema_version)';
+    end if;
+end;
+
+declare
+    c int;
+begin
+    select count(*) into c from user_tables where table_name = 'schema_version';
+    if c = 1 then
         EXECUTE IMMEDIATE 'INSERT INTO flyway_schema_history (SELECT * FROM schema_version)';
     end if;
 end;
+
