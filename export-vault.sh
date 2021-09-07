f8a8fb3760aaa46660ef68c0e8f3a78fe4c4890d
Michal J. Sladek <mrsladek@users.noreply.github.com>
2021-09-07 10:44:41 +0200

    Flytter db konfig tilbake til vault (#537)

--- export-vault.sh
+++ export-vault.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if test -f /var/run/secrets/nais.io/defaultDSconfig/jdbc_url;
+then
+  export DEFAULTDS_URL=$(cat /var/run/secrets/nais.io/defaultDSconfig/jdbc_url)
+  echo "Setting DEFAULTDS_URL to $DEFAULTDS_URL"
+fi
+
 if test -f /var/run/secrets/nais.io/defaultDS/username;
 then
   export DEFAULTDS_USERNAME=$(cat /var/run/secrets/nais.io/defaultDS/username)
