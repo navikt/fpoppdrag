df014df4d57edec0fcced22a58e92dc69ff0218f
Espen Waaga <espen.waaga@nav.no>
2022-11-11 09:53:39 +0100

    Legger på logging av request/respons fra oppdrags ws for debugging i dev

--- integrasjon/src/main/java/no/nav/foreldrepenger/oppdrag/OppdragConsumerConfig.java
+++ integrasjon/src/main/java/no/nav/foreldrepenger/oppdrag/OppdragConsumerConfig.java
@@ -4,10 +4,13 @@ import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 import javax.xml.namespace.QName;
 
+import org.apache.cxf.ext.logging.LoggingInInterceptor;
+import org.apache.cxf.ext.logging.LoggingOutInterceptor;
 import org.apache.cxf.feature.LoggingFeature;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.apache.cxf.ws.addressing.WSAddressingFeature;
 
+import no.nav.foreldrepenger.konfig.Environment;
 import no.nav.foreldrepenger.konfig.KonfigVerdi;
 import no.nav.system.os.eksponering.simulerfpservicewsbinding.SimulerFpService;
 import no.nav.vedtak.felles.integrasjon.felles.ws.CallIdOutInterceptor;
@@ -19,6 +22,8 @@ public class OppdragConsumerConfig {
     private static final String NAMESPACE = "http://nav.no/system/os/eksponering/simulerFpServiceWSBinding";
     private static final QName SERVICE = new QName(NAMESPACE, "simulerFpService");
     private static final QName PORT = new QName(NAMESPACE, "simulerFpServicePort");
+
+    private static final Environment ENV = Environment.current();
 
     private String endpointUrl; // NOSONAR
 
@@ -37,6 +42,17 @@ public class OppdragConsumerConfig {
         factoryBean.getFeatures().add(new WSAddressingFeature());
         factoryBean.getFeatures().add(new LoggingFeature());
         factoryBean.getOutInterceptors().add(new CallIdOutInterceptor());
+
+        if (!ENV.isProd()) {
+            var loggingInInterceptor = new LoggingInInterceptor();
+            loggingInInterceptor.setPrettyLogging(true);
+            var loggingOutInterceptor = new LoggingOutInterceptor();
+            loggingOutInterceptor.setPrettyLogging(true);
+            factoryBean.getInInterceptors().add(loggingInInterceptor);
+            factoryBean.getInFaultInterceptors().add(loggingInInterceptor);
+            factoryBean.getOutInterceptors().add(loggingOutInterceptor);
+            factoryBean.getOutFaultInterceptors().add(loggingInInterceptor);
+        }
         return factoryBean.create(SimulerFpService.class);
     }
 
