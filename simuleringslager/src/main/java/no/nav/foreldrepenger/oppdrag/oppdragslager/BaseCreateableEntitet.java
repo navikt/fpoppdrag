8abc66a26e02db1b544e99b0411d313aeaeefb21
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2023-02-20 22:05:27 +0100

    Felles 4225 task 3119 bruk kontekst (#892)

--- simuleringslager/src/main/java/no/nav/foreldrepenger/oppdrag/oppdragslager/BaseCreateableEntitet.java
+++ simuleringslager/src/main/java/no/nav/foreldrepenger/oppdrag/oppdragslager/BaseCreateableEntitet.java
@@ -2,13 +2,15 @@ package no.nav.foreldrepenger.oppdrag.oppdragslager;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
 
-import no.nav.vedtak.sikkerhet.context.SubjectHandler;
+import no.nav.vedtak.sikkerhet.kontekst.Kontekst;
+import no.nav.vedtak.sikkerhet.kontekst.KontekstHolder;
 
 /**
  * En basis {@link Entity} klasse som håndtere felles standarder for utformign
@@ -40,7 +42,7 @@ public abstract class BaseCreateableEntitet implements Serializable {
     }
 
     protected static String finnBrukernavn() {
-        var brukerident = SubjectHandler.getSubjectHandler().getUid();
-        return brukerident != null ? brukerident : BRUKERNAVN_NÅR_SIKKERHETSKONTEKST_IKKE_FINNES;
+        return Optional.ofNullable(KontekstHolder.getKontekst()).map(Kontekst::getKompaktUid)
+            .orElse(BRUKERNAVN_NÅR_SIKKERHETSKONTEKST_IKKE_FINNES);
     }
 }
