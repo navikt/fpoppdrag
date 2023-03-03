bab6487d9e4ef0cf74dac9dfa26a8234b07eb3ec
Michal J. Sladek <mrsladek@users.noreply.github.com>
2023-03-03 23:25:25 +0100

    Fix: sonar bug (#905)

--- web/src/main/java/no/nav/foreldrepenger/oppdrag/web/app/tjenester/simulering/SimuleringResultatTjeneste.java
+++ web/src/main/java/no/nav/foreldrepenger/oppdrag/web/app/tjenester/simulering/SimuleringResultatTjeneste.java
@@ -8,11 +8,9 @@ import javax.inject.Inject;
 
 import no.nav.foreldrepenger.oppdrag.domenetjenester.simulering.BeregningResultat;
 import no.nav.foreldrepenger.oppdrag.domenetjenester.simulering.FeilutbetalingTjeneste;
-import no.nav.foreldrepenger.oppdrag.domenetjenester.simulering.Oppsummering;
 import no.nav.foreldrepenger.oppdrag.domenetjenester.simulering.SimuleringBeregningTjeneste;
 import no.nav.foreldrepenger.oppdrag.domenetjenester.simulering.SimulertBeregningResultat;
 import no.nav.foreldrepenger.oppdrag.domenetjenester.simulering.dto.FeilutbetaltePerioderDto;
-import no.nav.foreldrepenger.oppdrag.oppdragslager.simulering.SimuleringGrunnlag;
 import no.nav.foreldrepenger.oppdrag.oppdragslager.simulering.SimuleringRepository;
 import no.nav.foreldrepenger.oppdrag.web.app.tjenester.simulering.dto.SimuleringDto;
 import no.nav.foreldrepenger.oppdrag.web.app.tjenester.simulering.dto.SimuleringResultatDto;
@@ -38,15 +36,20 @@ public class SimuleringResultatTjeneste {
     }
 
     public Optional<SimuleringResultatDto> hentResultatFraSimulering(Long behandlingId) {
-        Optional<SimulertBeregningResultat> simulertBeregningResultat = hentResultat(behandlingId);
+        var simulertBeregningResultat = hentResultat(behandlingId);
+
         if (simulertBeregningResultat.isPresent()) {
+            var resultat = simulertBeregningResultat.get();
             BeregningResultat beregningResultat;
-            boolean slåttAvInntrekk = skalKunViseResultatUtenInntrekk(simulertBeregningResultat);
-            if (slåttAvInntrekk) {
-                beregningResultat = simulertBeregningResultat.get().getBeregningResultatUtenInntrekk().get();
+            var resultatUtenInntrekk = resultat.getBeregningResultatUtenInntrekk();
+            var slåttAvInntrekk = skalKunViseResultatUtenInntrekk(resultat);
+
+            if (resultatUtenInntrekk.isPresent() && slåttAvInntrekk) {
+                beregningResultat = resultatUtenInntrekk.get();
             } else {
-                beregningResultat = simulertBeregningResultat.get().getBeregningResultat();
+                beregningResultat = resultat.getBeregningResultat();
             }
+
             return Optional.of(SimuleringResultatDto.builder()
                     .medSumFeilutbetaling(beregningResultat.getOppsummering().getFeilutbetaling())
                     .medSumInntrekk(beregningResultat.getOppsummering().getInntrekkNesteUtbetaling())
@@ -57,43 +60,41 @@ public class SimuleringResultatTjeneste {
     }
 
     public Optional<SimuleringDto> hentDetaljertSimuleringsResultat(Long behandlingId) {
-        Optional<SimulertBeregningResultat> simulertBeregningResultat = hentResultat(behandlingId);
-        if (skalKunViseResultatUtenInntrekk(simulertBeregningResultat)) {
+        var simulertBeregningResultat = hentResultat(behandlingId);
+        if (simulertBeregningResultat.isPresent() && skalKunViseResultatUtenInntrekk(simulertBeregningResultat.get())) {
             return Optional.of(SimuleringResultatMapper.map(hentNavnTjeneste, brukKunResultatUtenInntrekk(simulertBeregningResultat.get()), true));
         }
         return simulertBeregningResultat.map(resultat -> SimuleringResultatMapper.map(hentNavnTjeneste, resultat, false));
     }
 
-    private boolean skalKunViseResultatUtenInntrekk(Optional<SimulertBeregningResultat> simulertBeregningResultat) {
-        return simulertBeregningResultat.isPresent()
-                && erBådeInntrekkOgFeilutbetaling(simulertBeregningResultat.get())
-                && simulertBeregningResultat.get().getBeregningResultatUtenInntrekk().isPresent();
+    private boolean skalKunViseResultatUtenInntrekk(SimulertBeregningResultat simulertBeregningResultat) {
+        return erBådeInntrekkOgFeilutbetaling(simulertBeregningResultat);
     }
 
     public FeilutbetaltePerioderDto hentFeilutbetaltePerioder(Long behandlingId) {
-        Optional<SimuleringGrunnlag> optSimuleringGrunnlag = simuleringRepository.hentSimulertOppdragForBehandling(behandlingId);
+        var optSimuleringGrunnlag = simuleringRepository.hentSimulertOppdragForBehandling(behandlingId);
         if (optSimuleringGrunnlag.isEmpty()) {
             throw new TekniskException("FPO-319832", String.format("Fant ikke simuleringsresultat for behandlingId=%s", behandlingId));
         }
-        SimuleringGrunnlag simuleringGrunnlag = optSimuleringGrunnlag.get();
+        var simuleringGrunnlag = optSimuleringGrunnlag.get();
         return FeilutbetalingTjeneste.finnFeilutbetaltePerioderForForeldrepengerOgEngangsstønad(simuleringGrunnlag)
                 .orElseThrow(() -> new TekniskException("FPO-216725", String.format("Fant ingen perioder med feilutbetaling for bruker, behandlingId=%s", behandlingId)));
     }
 
     private Optional<SimulertBeregningResultat> hentResultat(Long behandlingId) {
-        Optional<SimuleringGrunnlag> optSimuleringGrunnlag = simuleringRepository.hentSimulertOppdragForBehandling(behandlingId);
+        var optSimuleringGrunnlag = simuleringRepository.hentSimulertOppdragForBehandling(behandlingId);
         if (optSimuleringGrunnlag.isPresent()) {
-            SimuleringGrunnlag simuleringGrunnlag = optSimuleringGrunnlag.get();
-            SimulertBeregningResultat resultat = simuleringBeregningTjeneste.hentBeregningsresultatMedOgUtenInntrekk(simuleringGrunnlag);
+            var simuleringGrunnlag = optSimuleringGrunnlag.get();
+            var resultat = simuleringBeregningTjeneste.hentBeregningsresultatMedOgUtenInntrekk(simuleringGrunnlag);
             return Optional.of(resultat);
         }
         return Optional.empty();
     }
 
     private boolean erBådeInntrekkOgFeilutbetaling(SimulertBeregningResultat resultat) {
-        Oppsummering oppsummering = resultat.getBeregningResultat().getOppsummering();
-        BigDecimal inntrekkNesteUtbetaling = oppsummering.getInntrekkNesteUtbetaling() != null ? oppsummering.getInntrekkNesteUtbetaling() : BigDecimal.ZERO;
-        BigDecimal feilutbetaling = oppsummering.getFeilutbetaling() != null ? oppsummering.getFeilutbetaling() : BigDecimal.ZERO;
+        var oppsummering = resultat.getBeregningResultat().getOppsummering();
+        var inntrekkNesteUtbetaling = oppsummering.getInntrekkNesteUtbetaling() != null ? oppsummering.getInntrekkNesteUtbetaling() : BigDecimal.ZERO;
+        var feilutbetaling = oppsummering.getFeilutbetaling() != null ? oppsummering.getFeilutbetaling() : BigDecimal.ZERO;
         return inntrekkNesteUtbetaling.compareTo(BigDecimal.ZERO) != 0 && feilutbetaling.compareTo(BigDecimal.ZERO) != 0;
     }
 
@@ -102,6 +103,4 @@ public class SimuleringResultatTjeneste {
                 .map(br -> new SimulertBeregningResultat(br, resultat.getGjelderYtelseType()))
                 .orElseThrow(() -> new IllegalStateException("Skal alltid ha resultat uten inntrekk her"));
     }
-
-
 }
