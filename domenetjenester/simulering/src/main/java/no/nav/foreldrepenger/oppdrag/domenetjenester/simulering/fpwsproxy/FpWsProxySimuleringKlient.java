24b9b568d0c9e9e3080c3d825bd6700000990128
espenwaaga <espen.waaga@nav.no>
2023-03-09 10:52:02 +0100

    Tar i bruk AZURE CC i klienten mot fpwsproxy

--- domenetjenester/simulering/src/main/java/no/nav/foreldrepenger/oppdrag/domenetjenester/simulering/fpwsproxy/FpWsProxySimuleringKlient.java
+++ domenetjenester/simulering/src/main/java/no/nav/foreldrepenger/oppdrag/domenetjenester/simulering/fpwsproxy/FpWsProxySimuleringKlient.java
@@ -30,7 +30,7 @@ import no.nav.vedtak.felles.integrasjon.rest.RestRequest;
 import no.nav.vedtak.felles.integrasjon.rest.TokenFlow;
 
 @ApplicationScoped
-@RestClientConfig(tokenConfig = TokenFlow.STS_CC, application = FpApplication.FPWSPROXY)
+@RestClientConfig(tokenConfig = TokenFlow.AZUREAD_CC, application = FpApplication.FPWSPROXY)
 public class FpWsProxySimuleringKlient {
 
     private final RestClient restClient;
