8bb3bd1ae151bba06d109b0d70e5590a4469190d
Michal J. Sladek <michal.sladek@nav.no>
2023-09-10 21:50:26 +0200

    Fikser tabeller

--- .oracle/oracle-init/fpoppdrag.sql
+++ .oracle/oracle-init/fpoppdrag.sql
@@ -1,35 +1,57 @@
--- ##################################################
--- ### Opplegg for enhetstester (lokal) ###
--- ##################################################
 DECLARE userexists INTEGER;
 BEGIN
-  SELECT count(*)
-  INTO userexists
-  FROM SYS.ALL_USERS
-  WHERE USERNAME = upper('fpoppdrag_unit');
-  IF (userexists = 0)
-  THEN
-    EXECUTE IMMEDIATE ('CREATE USER fpoppdrag_unit IDENTIFIED BY fpoppdrag_unit');
-  END IF;
+    SELECT count(*)
+    INTO userexists
+    FROM SYS.ALL_USERS
+    WHERE USERNAME = 'FPOPPDRAG';
+    IF (userexists = 0)
+    THEN
+        EXECUTE IMMEDIATE ('CREATE USER FPOPPDRAG IDENTIFIED BY fpoppdrag PROFILE DEFAULT ACCOUNT UNLOCK');
+    END IF;
 END;
 /
 
-GRANT CONNECT, RESOURCE, CREATE JOB, CREATE TABLE, CREATE SYNONYM, CREATE VIEW, CREATE MATERIALIZED VIEW TO fpoppdrag_unit;
+GRANT
+    CREATE SESSION,
+    ALTER SESSION,
+    CONNECT,
+    RESOURCE,
+    CREATE MATERIALIZED VIEW,
+    CREATE JOB,
+    CREATE TABLE,
+    CREATE SYNONYM,
+    CREATE VIEW,
+    CREATE SEQUENCE,
+    UNLIMITED TABLESPACE,
+    SELECT ANY TABLE
+    TO FPOPPDRAG;
 
--- ###############################
--- ### Opplegg for lokal jetty ###
--- ###############################
+ALTER USER FPOPPDRAG QUOTA UNLIMITED ON SYSTEM;
+
 DECLARE userexists INTEGER;
 BEGIN
-  SELECT count(*)
-  INTO userexists
-  FROM SYS.ALL_USERS
-  WHERE USERNAME = 'FPOPPDRAG';
-  IF (userexists = 0)
-  THEN
-    EXECUTE IMMEDIATE ('CREATE USER FPOPPDRAG IDENTIFIED BY fpoppdrag');
-  END IF;
+    SELECT count(*)
+    INTO userexists
+    FROM SYS.ALL_USERS
+    WHERE USERNAME = 'FPOPPDRAG_UNIT';
+    IF (userexists = 0)
+    THEN
+        EXECUTE IMMEDIATE ('CREATE USER FPOPPDRAG_UNIT IDENTIFIED BY fpoppdrag_unit PROFILE DEFAULT ACCOUNT UNLOCK');
+    END IF;
 END;
 /
 
-GRANT CONNECT, RESOURCE, CREATE JOB, CREATE TABLE, CREATE SYNONYM, CREATE VIEW, CREATE MATERIALIZED VIEW TO FPOPPDRAG;
+GRANT
+    CREATE SESSION,
+    ALTER SESSION,
+    CONNECT,
+    RESOURCE,
+    CREATE MATERIALIZED VIEW,
+    CREATE JOB,
+    CREATE TABLE,
+    CREATE SYNONYM,
+    CREATE VIEW,
+    CREATE SEQUENCE,
+    UNLIMITED TABLESPACE,
+    SELECT ANY TABLE
+    TO FPOPPDRAG_UNIT;
