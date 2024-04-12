aa76914b445cd23654b7fff286e5bbaaf4784393
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2024-04-12 09:02:33 +0200

    Bom 320, Jakarta EL for EE10 (#1041)

--- web/src/main/java/no/nav/foreldrepenger/oppdrag/web/app/metrics/PrometheusRestService.java
+++ web/src/main/java/no/nav/foreldrepenger/oppdrag/web/app/metrics/PrometheusRestService.java
@@ -1,16 +1,10 @@
 package no.nav.foreldrepenger.oppdrag.web.app.metrics;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
-
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.Response;
-
-import io.prometheus.client.CollectorRegistry;
-import io.swagger.v3.oas.annotations.Operation;
+import no.nav.vedtak.log.metrics.MetricsUtil;
 
 @Path("/metrics")
 @ApplicationScoped
@@ -19,13 +13,8 @@ public class PrometheusRestService {
     @GET
     @Operation(tags = "metrics", hidden = true)
     @Path("/prometheus")
-    public Response prometheus() {
-        try (final Writer writer = new StringWriter()) {
-            TextFormatter.write004(writer, CollectorRegistry.defaultRegistry.metricFamilySamples());
-            return Response.ok().encoding("UTF-8").entity(writer.toString()).header("content-type", TextFormatter.CONTENT_TYPE_004).build();
-        } catch (IOException e) {
-            throw new IllegalStateException(e);
-        }
+    public String prometheus() {
+        return MetricsUtil.scrape();
     }
 }
 
